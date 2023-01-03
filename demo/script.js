85ea82732d494c541b69ef2729e7d89c40e5e99e
Alessio <alessio.velluso@gmail.com>
2023-01-03 22:52:46 +0100

    Edited js

--- demo/script.js
+++ demo/script.js
@@ -3,34 +3,43 @@ Do that only if your script tag in HTML is type="module",
 otherwise copy and past the full function */
 
 
-const logo = document.querySelector('.logo');
+const logo = document.querySelector('.logo'); //MUST BE INSIDE A CONTAINER
 
-const hover3DEffect = (target, degrees) => {
-    target.style.transition = '150ms transform ease';
-    target.addEventListener('mousemove', e => {
-      const targetXPosition = e.target.getBoundingClientRect().left;
-      const targetYPosition = e.target.getBoundingClientRect().top;
-      const coordinatesFromCenter3D = {
-        y: ((e.clientX - targetXPosition) / target.clientWidth) * 2 - 1,
-        x: ((e.clientY - targetYPosition) / target.clientHeight)  * 2 - 1
-      }
+const hover3DEffect = (target, degrees, view = '2000px') => {
+  let transitioning = false;
+  target.parentElement.style.perspective = view
+  target.style.transition = '150ms transform ease';
+
+  target.addEventListener('mousemove', e => {
+    if (transitioning) return;
+    // When leaving mouse, this become true for a while so there's no evident bug
+
+    const targetXPosition = e.target.getBoundingClientRect().left;
+    const targetYPosition = e.target.getBoundingClientRect().top;
+    const coordinatesFromCenter3D = {
+      x: ((e.clientX - targetXPosition) / target.clientWidth) * 2 - 1,
+      y: ((e.clientY - targetYPosition) / target.clientHeight)  * 2 - 1
+    }  // Set the mouse coordinates so that they goes from -1 to 1, x and y axis
       
-      const rotateElement = () => {
-        const effect = {
-          x: coordinatesFromCenter3D.x * degrees,
-          y: coordinatesFromCenter3D.y * degrees,
-        }
-        logo.style.transform  = `rotateY(${-(effect.x)}deg) rotateX(${-(effect.y)}deg)`
-        //console.log(logo.style.transform);
+    const rotateElement = () => {
+      const effect = {
+        x: coordinatesFromCenter3D.x * degrees,
+        y: coordinatesFromCenter3D.y * degrees,
       }
-      rotateElement();
+      target.style.transform  = `rotateY(${(effect.x)}deg) rotateX(${-(effect.y)}deg)`
+    }
+    rotateElement(); // Rotate based on the degrees that the dev wants
       
-    });
+  });
   
-    target.addEventListener ('mouseleave', () => {
-      target.style.transform  = `rotateX(0deg) rotateY(0deg)`
-    });
+  target.addEventListener ('mouseleave', () => {
+    transitioning = true;
+    setTimeout(() => transitioning = false, 250); // Avoid any evident bug;
+
+    setTimeout(() => target.style.transform  = `rotateX(0deg) rotateY(0deg)`, 50)
+  });
 }
+
 
 
 hover3DEffect(logo, 20);
--- hover3DEffect.js
+++ hover3DEffect.js
@@ -21,30 +21,39 @@ USE IT:
 /* ----------------------------------------------------------------- */
 
 
-const hover3DEffect = (target, degrees) => {
+const hover3DEffect = (target, degrees, view = '2000px') => {
+  let transitioning = false;
+  target.parentElement.style.perspective = view
   target.style.transition = '150ms transform ease';
+
   target.addEventListener('mousemove', e => {
+    if (transitioning) return;
+    // When leaving mouse, this become true for a while so there's no evident bug
+
     const targetXPosition = e.target.getBoundingClientRect().left;
     const targetYPosition = e.target.getBoundingClientRect().top;
     const coordinatesFromCenter3D = {
-      y: ((e.clientX - targetXPosition) / target.clientWidth) * 2 - 1,
-      x: ((e.clientY - targetYPosition) / target.clientHeight)  * 2 - 1
-    }
-    
+      x: ((e.clientX - targetXPosition) / target.clientWidth) * 2 - 1,
+      y: ((e.clientY - targetYPosition) / target.clientHeight)  * 2 - 1
+    }  // Set the mouse coordinates so that they goes from -1 to 1, x and y axis
+      
     const rotateElement = () => {
       const effect = {
         x: coordinatesFromCenter3D.x * degrees,
         y: coordinatesFromCenter3D.y * degrees,
       }
-      logo.style.transform  = `rotateY(${-(effect.x)}deg) rotateX(${-(effect.y)}deg)`
-      //console.log(logo.style.transform);
+      target.style.transform  = `rotateY(${(effect.x)}deg) rotateX(${-(effect.y)}deg)`
     }
-    rotateElement();
-    
+    rotateElement(); // Rotate based on the degrees that the dev wants
+      
   });
-
+  
   target.addEventListener ('mouseleave', () => {
-    target.style.transform  = `rotateX(0deg) rotateY(0deg)`
+    transitioning = true;
+    setTimeout(() => transitioning = false, 250); // Avoid any evident bug;
+
+    setTimeout(() => target.style.transform  = `rotateX(0deg) rotateY(0deg)`, 50);
+    //timeout for helping avoiding bad effects
   });
 }
 
